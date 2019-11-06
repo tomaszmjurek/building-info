3c7ed62d3568521f86c2c31a308f48bb8d494393
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2019-11-06 12:57:47 +0100

    room class implementation

--- src/main/java/gryffindor/buildinginfo/models/Room.java
+++ src/main/java/gryffindor/buildinginfo/models/Room.java
@@ -1,4 +1,76 @@
 package gryffindor.buildinginfo.models;
 
-public class Room {
+public class Room extends Location {
+
+    private Float x;
+    private Float y;
+    private Float height;
+    private Float heating;
+    private Float light;
+
+
+    public Float getArea(){
+        return this.x * this.y;
+    }
+
+    public Float getVolume(){
+        return this.x * this.y * this.height;
+    }
+
+    public Float avgHeating(){
+        return this.heating / this.getArea();
+    }
+
+    public Float avgLight(){
+        return this.light / this.getVolume();
+    }
+
+    public Room(Integer id, String name, Float x, Float y, Float height, Float heating, Float light){
+        super(id, name);
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.heating = heating;
+        this.light = light;
+    }
+
+    public Float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public Float getHeating() {
+        return heating;
+    }
+
+    public void setHeating(Float heating) {
+        this.heating = heating;
+    }
+
+    public Float getLumens() {
+        return light;
+    }
+
+    public void setLumens(Float light) {
+        this.light = light;
+    }
 }
