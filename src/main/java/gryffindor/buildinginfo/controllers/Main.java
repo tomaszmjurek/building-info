de4dee0f118a825624d1e8e36038b85a036b26f9
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2019-11-06 12:22:36 +0100

    healtcheck endpoint

--- src/main/java/gryffindor/buildinginfo/controllers/Main.java
+++ src/main/java/gryffindor/buildinginfo/controllers/Main.java
@@ -1,4 +1,21 @@
 package gryffindor.buildinginfo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
 public class Main {
+
+    @RequestMapping("/healthcheck")
+    public Map<String, Object> healthcheck(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "ok");
+
+        return response;
+    }
+
 }
