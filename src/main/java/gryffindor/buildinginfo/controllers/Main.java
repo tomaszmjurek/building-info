3d7962d47d308748e78e4cf75ffdbf5cb60676cc
Dominos <dominikst998@gmail.com>
2019-12-09 12:28:40 +0100

    add Heating Endpoint

--- src/main/java/gryffindor/buildinginfo/controllers/Main.java
+++ src/main/java/gryffindor/buildinginfo/controllers/Main.java
@@ -16,26 +16,9 @@ import org.slf4j.LoggerFactory;
 /**
  * REST Controller class
  * It enables web service communication
- */
-@RestController
-public class Main {
-
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
-
-    @RequestMapping("/healthcheck")
-    public Map<String, Object> healthcheck(){
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", "ok");
-
-        return response;
-    }
-
-	/**
-	 * Example Request Json Structure
-	 * 
-	 * {
-    	"buildings": [
+ * Example Request JSON Structure
+ * {
+    "buildings": [
         {
             "id": 11,
             "floors": [
@@ -96,10 +79,25 @@ public class Main {
             ]
         }	
         
-	]
-	}
-	 
-	*
+]
+}
+ */
+@RestController
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+    @RequestMapping("/healthcheck")
+    public Map<String, Object> healthcheck(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "ok");
+
+        return response;
+    }
+
+	/**
+	* Method : Post
 	* Example json response
 	{
     	"area": 500
@@ -135,7 +133,16 @@ public class Main {
 
         return response;
     }
-
+    /**
+	* Method : Post
+	* Example json response
+	{
+    	"volume": 500
+     }
+	*
+	*  URL: 127.0.0.1:8080/getVolume
+	*
+	**/
     @PostMapping("/getVolume")
 	public Map<String, Object> getVolume(@RequestBody String json){
 		Map<String, Object> response = new HashMap<>();
@@ -160,74 +167,7 @@ public class Main {
 		return response;
 	}
 	/**
-	 * Example Request Json Structure
-	 * 
-	 * {
-    	"buildings": [
-        {
-            "id": 11,
-            "floors": [
-                {
-                    "id": 21,
-					"rooms": [
-						{
-							"id": 31,
-							"area": 2.0,
-							"volume": 6.0,
-							"heating": 4.0,
-							"light": 5.0
-						}
-					]
-                },
-                {
-                    "id": 22,
-					"rooms": [
-						{
-							"id": 32,
-							"area": 42.0,
-							"volume": 336.0,
-							"heating": 9.0,
-							"light": 10.0
-						}
-					]
-                }
-            ]
-        },
-
-        {
-            "id": 12,
-            "floors": [
-                {
-                    "id": 23,
-					"rooms": [
-						{
-							"id": 33,
-							"area": 2.0,
-							"volume": 6.0,
-							"heating": 4.0,
-							"light": 5.0
-						}
-					]
-                },
-                {
-                    "id": 24,
-					"rooms": [
-						{
-							"id": 34,
-							"area": 42.0,
-							"volume": 336.0,
-							"heating": 9.0,
-							"light": 10.0
-						}
-					]
-                }
-            ]
-        }	
-        
-	]
-	}
-	 
-	* 
+	* Method: Post
 	* Example json response
 	*{
     	"light": 50
@@ -259,6 +199,43 @@ public class Main {
             }
 
             response.put("light", sum);
+        }
+
+        return response;
+    }
+    /**
+	* Method: Post
+	* Example json response
+	*{
+    	"heating": 50
+     }
+	*
+	* URL: 127.0.0.1:8080/avgHeating
+	*
+	**/
+    @PostMapping("/avgHeating")
+    public Map<String, Object> avgHeating(@RequestBody String json ) {
+        Map<String, Object> response = new HashMap<>();
+
+        ArrayList<Building> buildings = null;
+        try {
+            buildings = JSONToBuildingParser.getBuildings(json);
+            logger.info("Building parsed from json");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(buildings == null) {
+            response.put("error", "error");
+
+        } else {
+            Float sum = 0.0f;
+            for(Building building : buildings) {
+                sum += building.avgHeating();
+                logger.debug("Average heating sum changed to {}", sum);
+            }
+
+            response.put("heating", sum);
         }
 
         return response;
